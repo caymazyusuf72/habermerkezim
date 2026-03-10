eafaf14dfb15f675527a5a034f670cd61d0e480e
caymazyusuf72 <yusufcaymazer32@gmail.com>
2026-03-10 21:37:04 +0300

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.untitled"
     compileSdk = 36  // Android 15 - Paket uyumluluğu için gerekli
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
